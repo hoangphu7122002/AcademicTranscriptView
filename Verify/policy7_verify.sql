d8ab2cfb9b4dedc748a97063c638b90bfe822a8d
Nguyen Truong <binclct@gmail.com>
2023-05-12 04:00:34 +0700

    Add verify code for emp_PDT to insert score

--- Verify/policy7_verify.sql
+++ Verify/policy7_verify.sql
@@ -7,7 +7,14 @@ select * from atv.course_score;
 -- This will okay, because this is the course that of the current semester HK223 (course 44) - ('44', '3', '11', 'HK223');
 update atv.course_score set midterm = 10.0, final = 0 where id = 179;
 
--- insert score for current semester
+-- insert score for current semester course_id: 29, 30 -> OKAY
+INSERT INTO atv.course_score (id, course_id, student_id, midterm, final, score)
+VALUES (190, 30, 59, 5, 6, 5.7);
+
+-- Insert score for outer semester -> Error
+-- ('22', '11', '22', 'HK213');
+INSERT INTO atv.course_score (id, course_id, student_id, midterm, final, score)
+VALUES (191, 22, 59, 5, 6, 5.7);
 
 -- This will not okay because id=180 -> course_id: 45 - ('45', '5', '19', 'HK213');
 update atv.course_score set midterm = 10.0, final = 10.0 where id = 180;
